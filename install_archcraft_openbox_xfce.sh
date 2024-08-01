4198f5fba63f38d9c26952abf40c5afffd97ebc6
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-08-01 20:59:25 +0600

    Update install_archcraft_openbox_xfce.sh

--- install_archcraft_openbox_xfce.sh
+++ install_archcraft_openbox_xfce.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
 # Clone repo
-echo "Downloading repo..."
-exec git clone https://github.com/KaziRifatMorshed/Quote-of-my-day.git &
-exec cd Quote-of-my-day &
+# echo "Downloading repo..."
+# exec git clone https://github.com/KaziRifatMorshed/Quote-of-my-day.git &
+# exec cd Quote-of-my-day &
 
 # install or download library and dependencies
-if; then # incomplete
-    exec sudo pacman -S glfw &
-    exec git clone https://github.com/cococry/leif.git && cd leif/ && ./install.sh &
-    exec paru -S libclipboard-git &
-fi
+# if; then # incomplete
+#     exec sudo pacman -S glfw &
+#     exec git clone https://github.com/cococry/leif.git && cd leif/ && ./install.sh &
+#     exec paru -S libclipboard-git &
+# fi
 
 # Compile
 # if which clang >/dev/null 2>&1; then
